b50c08d540a58337893bd637f1836a17fbb4c218
Shelby Grasso <shelbyagrasso@gmail.com>
2024-09-10 21:23:12 -0400

    adjusted program to print multiple names

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,10 @@
 import sys
 
 # input to get user's name
-user_name = sys.argv[1]
+#user_name = sys.argv[1]
 # print greeting
-print(f"Hello, {user_name}!")
+#print(f"Hello, {user_name}!")
+
+# Print greeting for each name passed
+for i in range(1,len(sys.argv)):
+    print(f"Hello, {sys.argv[i]}!")
